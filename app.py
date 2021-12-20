ace041e5b83a84edfbea604726fa48ff04bf6b5f
Byron Krauskopf <byronkrauskopf@gmail.com>
2021-12-20 13:58:55 -0500

    update

--- app.py
+++ app.py
@@ -65,7 +65,6 @@ def temp_monthly():
     return jsonify(temps=temps)
 
 #Min and Max temp route
-@app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
